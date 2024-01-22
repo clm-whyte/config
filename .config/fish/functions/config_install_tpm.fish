818b693702590f943df30120089448d7bc89ee99
Calum Maciver-Whyte <clm.whyte@gmail.com>
2024-01-22 13:03:48 +0000

    add install for config_install_tpm

---
+++ .config/fish/functions/config_install_tpm.fish
@@ -0,0 +1,4 @@
+function config_install_tpm --wraps='git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm' --description 'alias config_install_tpm git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm $argv
+        
+end
