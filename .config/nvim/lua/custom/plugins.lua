2cd39e57bc58a0b8390c37c0f4f901eb4598c36f
Calum Maciver-Whyte <clm.whyte@gmail.com>
2024-01-22 11:37:25 +0000

    add nvim config files back in

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,27 @@
+local plugins = {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettierd",
+        "typescript-language-server",
+      }
+    }
+  }
+}
+return plugins
