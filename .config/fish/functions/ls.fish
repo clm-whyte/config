23221006973354501aebc7a35c8ecad8cb0ae07e
clm-whyte <clm.whyte@gmail.com>
2024-01-22 10:06:21 +0000

    add aliases for eza

---
+++ .config/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza --oneline --all --long --header --icons --git' --description 'alias ls eza --oneline --all --long --header --icons --git'
+  eza --oneline --all --long --header --icons --git $argv
+        
+end
---
+++ .config/fish/functions/lst.fish
@@ -0,0 +1,4 @@
+function lst --wraps='eza --oneline --all --long --header --icons --git --tree' --description 'alias lst eza --oneline --all --long --header --icons --git --tree'
+  eza --oneline --all --long --header --icons --git --tree $argv
+        
+end
